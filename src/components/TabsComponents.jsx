7058b79bc854276e9b8f9ae79fbf66cb6b7e2a2a
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2024-11-21 03:23:09 +0330

    first

--- src/components/TabsComponents.jsx
+++ src/components/TabsComponents.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import React, { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 const TabsComponents = () => {
+  const [name, setName] = useState("");
+  const [ordering, setOrdering] = useState(0);
+
   return (
     <div className="w-100 shadow-lg">
       <Tabs>
@@ -20,9 +25,41 @@ const TabsComponents = () => {
           </Tab>
         </TabList>
 
-        <TabPanel >
-          <div className="panel-content h-100 d-flex align-items-center justify-content-center border">
-            <h2>Any content 1</h2>
+        <TabPanel>
+          <div className="panel-content h-100 d-flex align-items-start justify-content-start border p-4">
+            <div className="d-flex flex-column align-items-start justify-content-between h-100 w-100">
+              <div className="d-flex gap-2">
+                <Button
+                  className="align-left rounded-3"
+                  label="ذخیره تغییرات"
+                  // onClick={handleSubmit}
+                />
+                <Button
+                  className="align-left rounded-3 bg-white text-dark border"
+                  label="لغو"
+                  // onClick={closeModal}
+                />
+              </div>
+              <div className="p-field d-flex flex-column mb-4 w-100">
+                {/* <label htmlFor="name">نام گروه</label> */}
+                <InputText
+                  className="rounded-2"
+                  placeholder="زیرگروه "
+                  autoComplete="false"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="p-field d-flex flex-column mb-4 d-none">
+                <label htmlFor="ordering">ترتیب</label>
+                <InputText
+                  id="ordering"
+                  value={ordering}
+                  onChange={(e) => setOrdering(e.target.value)}
+                />
+              </div>
+            </div>
           </div>
         </TabPanel>
         <TabPanel>
