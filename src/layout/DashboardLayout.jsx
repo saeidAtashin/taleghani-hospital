d09e5e5737b90c5948a7494e84627e4ffaa7ce10
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2025-02-15 17:55:50 +0330

    fixed names and sidevbar

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -34,7 +34,7 @@ function DashboardLayout() {
   }, [isExpanded, isMobileView, setItem]);
 
   const handleToggle = () => {
-    setIsExpanded(prev => !prev);
+    setIsExpanded((prev) => !prev);
   };
 
   // State to track which submenu is active
@@ -71,9 +71,11 @@ function DashboardLayout() {
             <i className="lni lni-grid-alt"></i>
           </button>
         </div>
-        <ul className="sidebar-nav">
+        <ul className={`sidebar-nav ${!isExpanded ? "text-center" : ""}`}>
           <a
-            className="sidebar-header cursor-pointer"
+            className={`sidebar-header cursor-pointer ${
+              !isExpanded ? "justify-content-center" : ""
+            }`}
             style={{ cursor: "pointer" }}
             href="/dashboard"
           >
@@ -84,39 +86,53 @@ function DashboardLayout() {
           <li className="sidebar-item">
             <a
               href="#"
-              className="sidebar-link has-dropdown"
+              className={`sidebar-link has-dropdown ${
+                !isExpanded ? "justify-content-center" : ""
+              }`}
               onClick={() => toggleSubmenu("patients")}
               aria-expanded={activeSubmenu === "patients"}
             >
-              <i className="fst-normal d-flex align-items-center justify-content-between ">
-                بیماران
-                <span>
-                  <img src="/images/dropdown.svg" alt="dd" />
-                </span>
+              <i
+                className={`fst-normal d-flex align-items-center ${
+                  !isExpanded
+                    ? "justify-content-center"
+                    : "justify-content-between"
+                }`}
+              >
+                {isExpanded ? "بیماران" : "بیماران"}
+                {isExpanded && (
+                  <span>
+                    <img src="/images/dropdown.svg" alt="dd" />
+                  </span>
+                )}
               </i>
             </a>
             {/* Inner list for بیماران (Patients) */}
             <ul
               className={`sidebar-dropdown list-unstyled ${
                 activeSubmenu === "patients" ? "show" : "d-none"
-              }`}
+              } ${!isExpanded ? "text-center" : ""}`}
             >
               <li className="sidebar-item minheight d-flex">
                 <a
                   href="/dashboard/patients-lists"
-                  className="sidebar-link w-100  d-flex w-100 gap-3"
+                  className={`sidebar-link w-100 d-flex gap-3 ${
+                    !isExpanded ? "justify-content-center" : ""
+                  }`}
                 >
                   <img className="" src="/images/users.svg" alt="users" />
-                  <span>لیست بیماران</span>
+                  {isExpanded && <span>لیست بیماران</span>}
                 </a>
               </li>
               <li className="sidebar-item minheight d-flex">
                 <a
                   href="/dashboard/register-patient"
-                  className="sidebar-link w-100  d-flex w-100 gap-3"
+                  className={`sidebar-link w-100 d-flex gap-3 ${
+                    !isExpanded ? "justify-content-center" : ""
+                  }`}
                 >
                   <img src="/images/plus.svg" alt="users" />
-                  <span>افزودن بیمار جدید</span>{" "}
+                  {isExpanded && <span>افزودن بیمار جدید</span>}
                 </a>
               </li>
             </ul>
@@ -126,15 +142,25 @@ function DashboardLayout() {
           <li className="sidebar-item">
             <a
               href="#"
-              className="sidebar-link has-dropdown"
+              className={`sidebar-link has-dropdown ${
+                !isExpanded ? "justify-content-center" : ""
+              }`}
               onClick={() => toggleSubmenu("reports")}
               aria-expanded={activeSubmenu === "reports"}
             >
-              <i className="fst-normal d-flex align-items-center justify-content-between">
-                <> گزارش</>
-                <span>
-                  <img src="/images/dropdown.svg" alt="dd" />
-                </span>
+              <i
+                className={`fst-normal d-flex align-items-center ${
+                  !isExpanded
+                    ? "justify-content-center"
+                    : "justify-content-between"
+                }`}
+              >
+                {isExpanded ? "گزارش" : "گزارش"}
+                {isExpanded && (
+                  <span>
+                    <img src="/images/dropdown.svg" alt="dd" />
+                  </span>
+                )}
               </i>
             </a>
             <ul
@@ -143,10 +169,12 @@ function DashboardLayout() {
               <li className="sidebar-item minheight d-flex">
                 <a
                   href="/dashboard/reports"
-                  className="sidebar-link w-100 d-flex w-100 gap-3"
+                  className={`sidebar-link w-100 d-flex gap-3 ${
+                    !isExpanded ? "justify-content-center" : ""
+                  }`}
                 >
                   <img src="/images/users.svg" alt="report" />
-                  <span> مشاهده گزارشات</span>
+                  {isExpanded && <span> مشاهده گزارشات</span>}
                 </a>
               </li>
             </ul>
@@ -156,29 +184,41 @@ function DashboardLayout() {
           <li className="sidebar-item">
             <a
               href="#"
-              className="sidebar-link has-dropdown"
+              className={`sidebar-link has-dropdown ${
+                !isExpanded ? "justify-content-center" : ""
+              }`}
               onClick={() => toggleSubmenu("users")}
               aria-expanded={activeSubmenu === "users"}
             >
-              <i className="fst-normal d-flex align-items-center justify-content-between">
-                کاربران <span className="">و دسترسی‌ها</span>
-                <span>
-                  <img src="/images/dropdown.svg" alt="dd" />
-                </span>
+              <i
+                className={`fst-normal d-flex align-items-center ${
+                  !isExpanded
+                    ? "justify-content-center"
+                    : "justify-content-between"
+                }`}
+              >
+                {isExpanded ? "کاربران و دسترسی‌ها" : "کاربران"}
+                {isExpanded && (
+                  <span>
+                    <img src="/images/dropdown.svg" alt="dd" />
+                  </span>
+                )}
               </i>
             </a>
             <ul
               className={`sidebar-dropdown list-unstyled ${
                 activeSubmenu === "users" ? "show" : "d-none"
-              }`}
+              } ${!isExpanded ? "text-center" : ""}`}
             >
               <li className="sidebar-item minheight d-flex">
                 <a
                   href="/dashboard/user-management"
-                  className="sidebar-link w-100 d-flex w-100 gap-3"
+                  className={`sidebar-link w-100 d-flex gap-3 ${
+                    !isExpanded ? "justify-content-center" : ""
+                  }`}
                 >
                   <img src="/images/users.svg" alt="user" />
-                  <span>مدیریت کاربران</span>{" "}
+                  {isExpanded && <span>مدیریت کاربران</span>}
                 </a>
               </li>
             </ul>
@@ -188,46 +228,66 @@ function DashboardLayout() {
           <li className="sidebar-item">
             <a
               href="#"
-              className="sidebar-link has-dropdown"
+              className={`sidebar-link has-dropdown ${
+                !isExpanded ? "justify-content-center" : ""
+              }`}
               onClick={() => toggleSubmenu("definitions")}
               aria-expanded={activeSubmenu === "definitions"}
             >
-              <i className="fst-normal d-flex align-items-center justify-content-between">
-                <div>
-                  تعاریف <span>پایه</span>
-                </div>
-                <span className="flex">
-                  <img src="/images/dropdown.svg" alt="dd" className="" />
-                </span>
+              <i
+                className={`fst-normal d-flex align-items-center ${
+                  !isExpanded
+                    ? "justify-content-center"
+                    : "justify-content-between"
+                }`}
+              >
+                {isExpanded ? (
+                  <>
+                    <div>
+                      تعاریف <span>پایه</span>
+                    </div>
+                  </>
+                ) : (
+                  <div>تعاریف</div>
+                )}
+                {isExpanded && (
+                  <span className="flex">
+                    <img src="/images/dropdown.svg" alt="dd" className="" />
+                  </span>
+                )}
               </i>
             </a>
             <ul
               className={`sidebar-dropdown list-unstyled ${
                 activeSubmenu === "definitions" ? "show" : "d-none"
-              }`}
+              } ${!isExpanded ? "text-center" : ""}`}
             >
               <li className="sidebar-item minheight d-flex">
                 <a
                   href="/dashboard/basic-patient-definitions"
-                  className="sidebar-link w-100  d-flex w-100 gap-3"
+                  className={`sidebar-link w-100 d-flex gap-3 ${
+                    !isExpanded ? "justify-content-center" : ""
+                  }`}
                 >
                   <img src="/images/users.svg" alt="definitions" />
-                  <span>اطلاعات ثبت نام بیمار</span>
+                  {isExpanded && <span>اطلاعات ثبت نام بیمار</span>}
                 </a>
               </li>
             </ul>
             <ul
               className={`sidebar-dropdown list-unstyled ${
                 activeSubmenu === "definitions" ? "show" : "d-none"
-              }`}
+              } ${!isExpanded ? "text-center" : ""}`}
             >
               <li className="sidebar-item minheight d-flex">
                 <a
                   href="/dashboard/basic-test-definitions"
-                  className="sidebar-link w-100  d-flex w-100 gap-3"
+                  className={`sidebar-link w-100 d-flex gap-3 ${
+                    !isExpanded ? "justify-content-center" : ""
+                  }`}
                 >
                   <img src="/images/test.svg" alt="definitions" />
-                  <span>اطلاعات آزمایشات</span>
+                  {isExpanded && <span>اطلاعات آزمایشات</span>}
                 </a>
               </li>
             </ul>
@@ -236,7 +296,9 @@ function DashboardLayout() {
         <div className="sidebar-footer">
           <a
             href="/"
-            className={`sidebar-link d-flex items-center justify-content-around`}
+            className={`sidebar-link d-flex ${
+              !isExpanded ? "justify-content-center" : "justify-content-around"
+            }`}
           >
             <span className={`${isExpanded ? "d-block" : "d-none"}`}>
               Logout
