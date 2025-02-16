1ac13f7ea3dcd38c55c0de0d565887b2ba12874c
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2025-02-16 21:12:46 +0330

    dlt extra

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import Lottie from "react-lottie-player";
-import animationData from "../animation-json/developing.json";
-import HeaderName from "../components/HeaderName";
-import { DatePicker } from "zaman";
 
 const DashboardPage = () => {
   return (
     <div className="screen-width d-flex flex-column justify-content-center mx-auto align-items-center">
-      <div className=" position-fixed top-0 position-relative">
-        <HeaderName
-          HeaderName={"در حال توسعه"}
-          className={"mt-5 z-3 text-center position-absolute "}
-        />
-        <Lottie className="" loop play animationData={animationData} />
-      </div>
+      <div className=" position-fixed top-0 position-relative"></div>
     </div>
   );
 };
