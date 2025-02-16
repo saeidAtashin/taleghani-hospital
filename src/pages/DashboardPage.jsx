a9a1f17eb125a26e3b1c736cf4211abe83478212
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2025-02-16 21:26:06 +0330

    UI change

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -35,36 +35,41 @@ const DashboardPage = () => {
   ];
 
   return (
-    <div className="container mt-4">
+    <div className="container mt-5 ">
       <div className="row">
         {dashboardData.map((card, index) => (
           <div key={index} className="col-md-5 mb-4">
-            <Card className="h-100 shadow-sm">
+            <Card className="h-100 shadow-lg rounded-3">
               <div className="d-flex flex-column h-100">
                 {/* Top Section */}
-                <div className="text-center mb-3 ">
+                <div className="text-center mb-3 d-flex justify-content-center align-items-center">
                   <i className={`${card.icon} fs-1 text-primary mb-2`}></i>
                   <h5 className="mb-2">{card.title}</h5>
-                  <h3 className="text-primary mb-0">{card.mainNumber}</h3>
                 </div>
-
+                <div className="text-center mb-3 d-flex justify-content-center align-items-center gap-2">
+                  <h3 className="text-primary mb-0">{card.mainNumber}</h3>{" "}
+                  <h3 className=" mb-0">نفر</h3>
+                </div>
                 {/* Divider */}
                 <hr className="my-3" />
-
                 {/* Bottom Section */}
                 <div className="d-flex justify-content-between mt-auto ">
                   <div className="text-end mx-auto">
                     <div className="text-muted small ">
                       {card.bottomRight.title}
                     </div>
-                    <div className="fw-bold text-center" >{card.bottomRight.number}</div>
+                    <div className="fw-bold text-center text-primary">
+                      {card.bottomRight.number}
+                    </div>
                   </div>
                   <div className="vr mx-2"></div>
                   <div className="text-end mx-auto">
                     <div className="text-muted small ">
                       {card.bottomLeft.title}
                     </div>
-                    <div className="fw-bold text-center">{card.bottomLeft.number}</div>
+                    <div className="fw-bold text-center text-primary">
+                      {card.bottomLeft.number}
+                    </div>
                   </div>
                 </div>
               </div>
